198f63d7facd4ef8850915972a8c49bd5b284e63
andrew.holt <ajh1143@uw.edu>
2019-05-02 16:24:54 -0700

    Created method 'binner' to separate data into a defined bin range.

---
+++ pandas_book.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+
+def binner(values, bins):
+    """ Group data according to a set bins
+    :param values: List of data values to be set to bins
+    :param bins: List of bins to be applied to values
+    :return:
+    """
+    if values and bins:
+        binned_data = pd.cut(values, bins)
+        print(f'Bins: {binned_data}')
+        return binned_data
+    else:
+        print('Error')
+
